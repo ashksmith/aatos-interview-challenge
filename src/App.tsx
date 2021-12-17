677093d8f38dc0f08e25e98681b7f7190e67132f
Smithy <mail@ashleyksmith.com>
2021-12-17 12:11:39 +0200

    Added some simple persistence

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,48 @@ import Header from "components/Header/Header";
 import { Cities } from "interfaces";
 
 const App: FunctionComponent = () => {
-  const [city, setCity] = useState<Cities>("Helsinki");
-  const prefersDark = window.matchMedia("prefer-color-scheme: dark").matches;
+  const lastChosenCity = localStorage.getItem("city");
+  // @ts-ignore
+  const [city, setCityF] = useState<Cities>(lastChosenCity || "Helsinki");
+
+  const prefersDark: boolean =
+    window.matchMedia("prefer-color-scheme: dark").matches ||
+    JSON.parse(String(localStorage.getItem("dark")));
   const [dark, setDark] = useState<boolean>(prefersDark);
-  const [imperial, setImperial] = useState<boolean>(false);
+
+  const prefersImperial: boolean = JSON.parse(
+    String(localStorage.getItem("imperial"))
+  );
+  const [imperial, setImperial] = useState<boolean>(prefersImperial);
+
+  const setDarkWithLocalStorage = () => {
+    setDark(!dark);
+    localStorage.setItem("dark", !dark ? "true" : "false");
+  };
+
+  const setImperialWithLocalStorage = () => {
+    setImperial(!imperial);
+    localStorage.setItem("imperial", !imperial ? "true" : "false");
+  };
+
+  const setCity = (city: Cities) => {
+    setCityF(city);
+    localStorage.setItem("city", city);
+  };
 
   return (
     <div className={`App ${dark ? "Dark" : "Light"}`}>
       <div className="SettingsContainer">
         <div className="Flex">
           <Typography.Title level={5}>Dark mode:</Typography.Title>
-          <Switch onChange={() => setDark(!dark)} />
+          <Switch onChange={() => setDarkWithLocalStorage()} checked={dark} />
         </div>
         <div className="Flex">
           <Typography.Title level={5}>Imperial:</Typography.Title>
-          <Switch onChange={() => setImperial(!imperial)} />
+          <Switch
+            onChange={() => setImperialWithLocalStorage()}
+            checked={imperial}
+          />
         </div>
       </div>
 
