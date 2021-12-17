308f3e91ce922a99dcdfd26bca83500891011629
Smithy <mail@ashleyksmith.com>
2021-12-17 09:47:21 +0200

    Added project_constants file

---
+++ src/project_constants.ts
@@ -0,0 +1,24 @@
+export const weekdays: string[] = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export const months: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
